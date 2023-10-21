9ea6ccf0ce420c585eb64fc7640b52577e30a65c
unknown <balaji.21bce8520@vitapstudent.ac.in>
2023-10-21 08:21:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import './App.css';
+import Agefinder, {Age} from "./components/Agefinder";
+function App() {
+  return (
+      <Agefinder/>
+  
+  );
+}
+
+export default App;
